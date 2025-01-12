ea3a25301a271326112861465e30ef34eb8a19f6
ThiagoMSLK <thiagolk2704@gmail.com>
2025-01-11 23:25:03 -0300

    ATT: test_project.py
    Criando contêineres para a seleção dos dados.
    Organizando o código.
    
    OBS: Por algum motivo quando pesquisa o ativo na box "Digite" o código baixa o DF corretamente porem não consegue gerar o gráfico

--- test_project.py
+++ test_project.py
@@ -1,6 +1,6 @@
 # 1.1 Importação das bibliotecas internas
 import datetime as dt
-from time import sleep
+# from time import sleep
 
 # 1.2 Importação das bibliotecas externas
 import pandas as pd
@@ -9,37 +9,76 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
+# 1.3 Difinição da Tuples OHLCV e do DataFrame Ticker
+OHLCV = ('Open', 'High', 'Low', 'Close', 'Volume')
 
-stocks = pd.read_csv('nasdaq_screener_10-01-2025.csv')['Symbol'].values
+ticker = pd.read_csv('nasdaq_screener_10-01-2025.csv')['Symbol'].values
 
-# 1.3 Difinição do começo e fim da busca
-# start_date = dt.datetime(2022, 1, 1)
-# end_date = dt.datetime.today()
+# 2.1 Configuração da página
+st.set_page_config(page_title='Análise de Dados Financeiros', layout='wide')
 
-start_date, end_date = st.slider(
+st.title('Análise de Dados Financeiros')
+
+# 1.3 Difinição dos dados que o usuário deseja visualizar
+
+with st.container():
+    st.header('Escolha os dados que deseja visualizar')
+
+    box1, box2, box3 = st.columns([1,1,2])
+    
+    # 1.4 Difinição do Ticker
+    with box1:
+
+        with st.container():
+            st.write('Escolha o ativo que deseja visualizar')
+            
+            col1, col2 =st.columns([1,1])
+            
+            with col1:
+                ticker_selecionado = st.selectbox("Selecione ou >", options= ticker)
+            with col2:
+                ticker_digitado = st.text_input("Digite")
+
+            if ticker_digitado:
+                selecao_ticker = ticker_digitado.upper()
+            else:
+                selecao_ticker = ticker_selecionado
+
+    # 1.5 Escolha do OHLCV
+    with box2:
+        with st.container():
+            st.write('Escolha os dados que deseja visualizar:')
+        selecao_ohlcv = st.selectbox(f'O tcker {selecao_ticker} foi escolhido', options=OHLCV)
+        
+    # 1.6 Difinição do começo e fim da busca
+    with box3:
+
+        # 1.6.1 Procurando o primeiro dia do ativo
+        ativo = yf.Ticker(selecao_ticker)
+        historico = ativo.history(period="max")
+        primeiro_dia = historico.index[0].date()
+
+        # 1.6.2 Escolha do intervalo de datas
+        start_date, end_date = st.slider(
     "Escolha o intervalo de datas:",
-    min_value=dt.date(2020, 1, 1),
+    min_value=None,
     max_value=dt.date.today(),
-    value=(dt.date(2023, 1, 1), dt.date.today()),
+    value=(primeiro_dia, dt.date.today()),
     format="YYYY-MM-DD"
 )
 
-# 2. Retornar as informações da API
-selecao_ticker = st.selectbox("Escolha a criptomoeda", options= stocks)
 
 
-df = yf.download(selecao_ticker, start=start_date, end=end_date)
+# 1.7 Retornar as informações da API
+df = yf.download(selecao_ticker, start=start_date, 
+        end=dt.date.today())
 
-sleep(1)
+# 2.1 Visualização dos dados
+st.write(f'## Dados do Ativo {selecao_ticker}')
 
-ohlcv = ['Open', 'High', 'Low', 'Close', 'Volume']
-
-selecao_ohlcv = st.selectbox('Escolha os dados que deseja visualizar', options=ohlcv)
-
-
-st.title(f'Análise de Dados do {selecao_ticker}')
-
+# 2.2 Visualização do gráfico
 st.line_chart(df[selecao_ohlcv])
 
+# 2.3 Visualização do DataFrame
 st.write(f'## Dados do {selecao_ticker}')
 st.write(df)
