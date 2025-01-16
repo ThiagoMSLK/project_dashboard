76b7221e7c6eb0e246697d22dca111d07dce8271
ThiagoMSLK <thiagolk2704@gmail.com>
2025-01-16 19:39:35 -0300

    ATT: test_project.py, project_dashboard.py, yfinance_test.py
    ATT: test_project.py
    1- Correção do bug que não exibia o gráfico de alguns ativos
    2 - Implementação da leitura e exibição da moeda de referencia do ativo no contêiner das métricas
    3 -  Remoção do do auto ajuste da coluna "Close" mas mantendo ela sem ajuste e adicionando a coluna "Adj Close" com os dados de fechamento ajustado
    
    ATT : project_dashboard.py
    Recebendo o código do test_project.py
    
    ATT: yfinance_test.py
    Primeiros testes na biblioteca YFinance

--- project_dashboard.py
+++ project_dashboard.py
@@ -12,7 +12,7 @@ import plotly.express as px
 
 
 # 1.3 Difinição da Tuples OHLCV e do DataFrame Ticker
-OHLCV = ("Open", "High", "Low", "Close", "Volume")
+OHLCV = ("Open", "High", "Low", "Close", "Adj Close", "Volume")
 
 ticker = pd.read_csv("nasdaq_screener_10-01-2025.csv")["Symbol"].values
 
@@ -22,10 +22,9 @@ st.set_page_config(page_title="Análise de Dados Financeiros", layout="wide")
 st.title("Análise de Dados Financeiros")
 
 # 1.3 Difinição dos dados que o usuário deseja visualizar
-
 with st.container():
-    st.header("Escolha os dados que deseja visualizar")
 
+    st.header("Escolha os dados que deseja visualizar")
     box1, box2, box3 = st.columns([1,1,2])
     
     # 1.4 Difinição do Ticker
@@ -55,23 +54,25 @@ with st.container():
     with box2:
         with st.container():
             st.write("Escolha os dados que deseja visualizar:")
-        selecao_ohlcv = st.selectbox(f"O tcker {selecao_ticker} foi escolhido", options=OHLCV)
+        selecao_ohlcv = st.selectbox(f"O Ticker {selecao_ticker} foi escolhido", options=OHLCV)
         
     # 1.6 Difinição do começo e fim da busca
     with box3:
+
         # 1.6.1 Colocando o o titulo, mas o princial objetivo é alinhar com as box
         with st.container():
             st.write("Escolha o intervalo de datas:")
 
-        # 1.6.2 Procurando o primeiro dia do ativo
-            # Mais explicativo
-        ativo = yf.Ticker(selecao_ticker)
-        historico = ativo.history(period="max")
-        primeiro_dia = historico.index[0].date()
-            # Mais simples e direto
-        # primeiro_dia = yf.Ticker(selecao_ticker).history(period="max").index[0].date()
+        # 1.6.2 Retornar as informações da API
+        df = yf.Ticker(selecao_ticker).history(period="max", auto_adjust=False)
 
-        # 1.6.3 Escolha do intervalo de datas e deixando o label vazio para alinhar com as box
+        # 1.6.3 Procurando a moeda do ativo
+        moeda = yf.Ticker(selecao_ticker).info["currency"]
+
+        # 1.6.3 Procurando o primeiro dia do ativo
+        primeiro_dia = df.index[0].date()
+
+        # 1.6.5 Escolha do intervalo de datas e deixando o label vazio para alinhar com as box
         start_date, end_date = st.slider(
     "",
     min_value=None,
@@ -80,29 +81,31 @@ with st.container():
     format="YYYY-MM-DD"
 )
 
-# 1.7 Retornar as informações da API
-df = yf.download(selecao_ticker, start=start_date, 
-        end=dt.date.today())
-
 # 2.1 Metricas
-ult_atualizacao = df.index.max().date()
-ult_cotacao = round(df.loc[df.index.max(), "Close"], 2).item()
-menor_cotacao = round(df["Close"].min(), 2).item()
-maior_cotacao = round(df["Close"].max(), 2).item()
-prim_cotacao = round(df.loc[df.index.min(), "Close"], 2).item()
-delta = round(((ult_cotacao - prim_cotacao)/prim_cotacao)*100, 2)
+ult_atualizacao = df.index.max().date() # Data da última atualização
+
+prim_cotacao = round(df.loc[df.index.min(), "Close"], 2).item() # Primeira cotação
+
+ult_cotacao = round(df.loc[df.index.max(), "Close"], 2).item() # Última cotação
+
+menor_cotacao = round(df["Close"].min(), 2).item() # Menor cotação
+
+maior_cotacao = round(df["Close"].max(), 2).item() # Maior cotação
+
+delta = round(((ult_cotacao - prim_cotacao)/prim_cotacao)*100, 2) # Variação percentual
+
 
 # 2.2 Visualização dos dados das metricas
 with st.container():
     box1, box2, box3 = st.columns([1,1,1])
     with box1:
-        st.metric(f"Última cotação: {ult_atualizacao}",f"US$ {ult_cotacao}",f"{delta}%")
+        st.metric(f"Última cotação: {ult_atualizacao}",f"{moeda} {ult_cotacao}",f"{delta}%")
 
     with box2:
-        st.metric("Maior cotação:",f"US$ {maior_cotacao}")
+        st.metric("Maior cotação:",f"{moeda} {maior_cotacao}")
 
     with box3:
-        st.metric("Menor cotação:",f"US$ {menor_cotacao}")
+        st.metric("Menor cotação:",f"{moeda} {menor_cotacao}")
 
 # 2.3 Visualização dos dados
 st.write(f"## Dados do Ativo {selecao_ticker}")
@@ -113,9 +116,12 @@ st.area_chart(df[selecao_ohlcv].astype(float))
 # 2.5 Visualização do DataFrame
 with st.container():
     box1, box2 = st.columns([1,1])
+
+    # DataFrame
     with box1:
         st.write(f"## Dados do {selecao_ticker}\n")
         st.write(df)
+    # Informações do DataFrame
     with box2:
         st.write(f"## Informaões dos Dados do {selecao_ticker}")
         st.write(df.describe())
